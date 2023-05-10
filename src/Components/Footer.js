dbe1d092e7908be311a73186dde367df460a5419
Divyanshu Verma <divyanshuvermaji@gmail.com>
2023-05-10 12:25:53 +0530

    initial commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,17 +1,9 @@
-import React from 'react';
+import React from "react";
 
 export default () => {
   return (
     <footer>
-      <div className="footer">
-        <ul>
-          <li>React JS</li>
-          <li>React Router</li>
-          <li>Draft JS</li>
-          <li>Love</li>
-          <li>By <a href="https://www.zonayed.me" rel="noopener noreferrer" target="_blank">Zonayed Ahmed</a></li>
-        </ul>
-      </div>
+      <div className="footer"></div>
     </footer>
-  )
-}
+  );
+};
